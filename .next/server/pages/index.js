c5f8cb4749059dc6916c469edc5a5971511cbddd
ShadWeb <shyifou@gmail.com>
2025-11-05 10:56:12 +0330

    up1

--- .next/server/pages/index.js
+++
@@ -1,9 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/index.js")
-R.c("server/chunks/ssr/node_modules_0b53a5be._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__9f350845._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__085b52c5._.js")
-R.c("server/chunks/ssr/[externals]_tesseract_c6dd006f.js")
-R.c("server/chunks/ssr/node_modules_50baac54._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__756a600e._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/pages/index.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/pages/_document.tsx [ssr] (ecmascript)\", INNER_APP => \"[project]/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/pages/index.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/pages/_document.tsx [ssr] (ecmascript)\", INNER_APP => \"[project]/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
